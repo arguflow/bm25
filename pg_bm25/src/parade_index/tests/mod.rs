be44998446d1095e67970d42141cb2874c945289
Nick K <nick@arguflow.gg>
2023-12-01 18:14:43 -0600

    cleanup: fully remove shared crate

--- pg_bm25/src/parade_index/tests/mod.rs
+++ pg_bm25/src/parade_index/tests/mod.rs
@@ -1,7 +1,38 @@
+use pgrx::Spi;
+
+pub fn dblink(query: &str) -> String {
+    // Retrieve the current database name from the PostgreSQL environment.
+    let current_db_name: String = Spi::get_one("SELECT current_database()::text")
+        .expect("couldn't get current database for postgres connection")
+        .unwrap();
+
+    // Retrieve the current port number on which the PostgreSQL server is listening.
+    let current_port: u32 =
+        Spi::get_one::<String>("SELECT setting FROM pg_settings WHERE name = 'port'")
+            .expect("couldn't get current port for postgres connection")
+            .unwrap()
+            .parse()
+            .expect("couldn't parse current port into u32");
+
+    // Prepare the connection string for dblink. This string contains the host (assumed to be
+    // localhost in this function), the port number, and the database name to connect to.
+    let connection_string = format!(
+        "host=localhost port={} dbname={}",
+        current_port, current_db_name
+    );
+
+    // Escape single quotes in the SQL query since it will be nested inside another SQL string
+    // in the dblink function call. Single quotes in SQL strings are escaped by doubling them.
+    let escaped_query_string = query.replace('\'', "''");
+
+    // Construct the dblink function call with the connection string and the escaped query.
+    // This function call is what can be executed within a PostgreSQL environment.
+    format!("dblink('{connection_string}', '{escaped_query_string}')")
+}
+
 #[pgrx::pg_schema]
 mod tests {
     use pgrx::*;
-    use shared::testing::dblink;
 
     const SETUP_SQL: &str = include_str!("tokenizer_chinese_compatible_setup.sql");
 
@@ -20,7 +51,7 @@ mod tests {
 
         // Set up the test environment using dblink to run the setup SQL in a separate connection.
         // The setup SQL is expected to prepare the database with the necessary configuration for the tokenizer.
-        let setup_query = format!("SELECT * FROM {} AS (_ text)", &dblink(SETUP_SQL));
+        let setup_query = format!("SELECT * FROM {} AS (_ text)", &super::dblink(SETUP_SQL));
         Spi::run(&setup_query).expect("error running dblink setup query");
     }
 }
--- shared/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod logs;
-pub mod testing;
-
-// We need to re-export the dependencies below, because they're used by our public macros.
-// This lets consumers of the macros use them without needing to also install these dependencies.
-pub use pgrx;
-pub use serde_json;
